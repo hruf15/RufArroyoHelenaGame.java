acdd7e758b042f86b4d5b46eb37c75e7ec66fbaf
hruf15 <helenarufarroyo@gmail.com>
2025-01-02 16:36:30 +0100

    primera actualización

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import  java.util.Scanner;
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+        File file = new File("ListadoPeliculas");
+        Scanner input = new Scanner(file);
+
+        System.out.println("");
+    }
+}
